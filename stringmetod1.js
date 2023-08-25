b9ca001f581e4933517114932a8dfbf3b6d1614c
yash <rajeshbhaiyash248@gmail.com>
2023-08-25 17:46:52 +0530

    hii

--- stringmetod1.js
+++ stringmetod1.js
@@ -1,30 +1,86 @@
 //let result
-let txt = "How are you guys?"
-let result = txt.length;
-console.log(result);
+//let txt = "How are you guys?"
+//let result = txt.length;
+//console.log(result);
 
 //slice
-result = txt.slice(5,9);
-result = txt.slice(6);
-result = txt.slice(-15,-9);
-console.log(result);
+//result = txt.slice(5,9);
+//result = txt.slice(6);
+//result = txt.slice(-15,-9);
+//console.log(result);
 
 //substr
-result = txt.substr(5,9);
-result = txt.substr(-15,3);
+//result = txt.substr(5,9);
+//result = txt.substr(-15,3);
 
 //substring
-result = txt.substring(-5,9);
-result = txt.substring(9);
+//result = txt.substring(-5,9);
+//result = txt.substring(9);
 
 //toUpperCase
-result = txt.toUpperCase();
+//result = txt.toUpperCase();
 
 //toLowerCase
-result = txt.toLowerCase();
+//result = txt.toLowerCase();
 
 //concat
-let a = "Hello";
-let b = "World";
-result = a.concat(" ",b);
+//let a = "Hello";
+//let b = "World";
+//result = a.concat(" ",b);
+//console.log(result);
+
+//replace
+//let data = 'Hii, how are you???,you are ready???';
+//let result;
+//data = data.replace("you","all");
+//console.log(data);
+//data = data.replace(/you/i,"all");
+//console.log(data);
+
+//replaceAll
+//data = data.replaceAll("you","all");
+//console.log(data);
+
+//trim
+//let a = "    hello     ";
+//console.log(a.length);
+//result = a.trim();
+//console.log(a.length);
+//console.log(a);
+
+//trim strat
+//let a = "    hello     ";
+//console.log(a.length);
+//result = a.trimStart();
+//console.log(a.length);
+//console.log(a);
+
+//trimEnd
+//let a = "    hello     ";
+//console.log(a.length);
+//result = a.trimEnd();
+//console.log(a.length);
+//console.log(a);
+
+//padstart
+//let b = "5";
+//result = b.padStart(5,"x");
+//console.log(result);
+
+//padEnd
+//result = b.padEnd(5,"x");
+//console.log(result);
+
+//charAt
+//let txt = "SkillQode";
+//result = txt.charAt(2);
+//console.log(result);
+
+//charcodeAt
+//result = txt.charCodeAt(5);
+//console.log(result);
+
+//split
+let data = "Hello world guys???";
+result = data.split(" ");
 console.log(result);
